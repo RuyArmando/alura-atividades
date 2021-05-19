2f1db83ac5b9e60841673265b44bd16ec5d247bd
Ruy Armando <ruyarmando@gmail.com>
2021-05-19 19:35:20 -0300

    Template Method - Aula 03

---
+++ source/ruyarmando.designpatterns/designpatterns/Models/Impostos/TemplateDeImpostoCondicional.cs
@@ -0,0 +1,23 @@
+﻿using designpatterns.Interfaces;
+
+namespace designpatterns.Models
+{
+    public abstract class TemplateDeImpostoCondicional : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (DeveUsarMaximaTaxacao(orcamento))
+            {
+                return MaximaTaxacao(orcamento);
+            }
+            else
+            {
+                return MinimaTaxacao(orcamento);
+            }
+        }
+
+        public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
+    }
+}
